ae5c2e428d27532f28e21ae9c3a486b6b5c597e4
Daniel Malý <malydani@fit.cvut.cz>
2015-11-05 09:30:25 +0100

    Pripojovani na databazi

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask import render_template
+import psycopg2
 
 app = Flask(__name__)
 
+conn = psycopg2.connect(database="asse", user="asse", password="zabalenymotylek2015")
+cur = conn.cursor()
 
 @app.route('/')
 def homepage():
