35355f525721d862946c5915d02eb5b0ce6af356
Daniel Maly <maly.dan@gmail.com>
2015-11-05 14:34:43 +0100

    Upload souborů pro hledání

--- webapp/static/search.js
+++ webapp/static/search.js
@@ -1,10 +1,19 @@
 $(function() {
     $('a#search').bind('click', function() {
-      $.getJSON($SCRIPT_ROOT + '/search', {
 
-      }, function(data) {
-        $("#result").text(data.result);
-      });
-      return false;
+        var fd = new FormData();
+        var file_data = $('input[type="file"]')[0].files; // for multiple files
+        fd.append("file", file_data[0]);
+
+        $.ajax({
+            url: $SCRIPT_ROOT + '/search',
+            processData: false,
+            contentType: false,
+            method: 'POST',
+            data: fd
+        }).done(function(data) {
+            $("#result").text(data.result);
+        });
+        return false;
     });
   });
--- webapp/webapp.py
+++ webapp/webapp.py
@@ -7,13 +7,27 @@ app = Flask(__name__)
 conn = psycopg2.connect(database="asse", user="asse", password="zabalenymotylek2015")
 cur = conn.cursor()
 
+ALLOWED_UPLOAD_EXTENSIONS = {'mp3', 'wav'}
+
+
 @app.route('/')
 def homepage():
     return render_template('index.html')
 
-@app.route('/search')
+
+@app.route('/search', methods=['POST'])
 def search():
-    return jsonify(result="Couldn't find anything lol")
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            return jsonify(result="Got your file okay")
+        else:
+            return jsonify(result="You nit.")
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_UPLOAD_EXTENSIONS
 
 
 if __name__ == '__main__':
