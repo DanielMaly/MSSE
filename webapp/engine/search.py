7ab272e8b5389eb3de3e352fe48188e62e395a11
Daniel Maly <maly.dan@gmail.com>
2015-11-30 22:45:04 +0100

    Search bere signatury z databáze

--- webapp/engine/search.py
+++ webapp/engine/search.py
@@ -1,31 +1,32 @@
 __author__ = 'dm'
 
 import os
-import glob
 import heapq
 import numpy
 import scipy.stats as stats
+import db.database as db
 
 SIGNATURE_PARENT_DIR = os.path.join("data", "signatures")
 
 
 def search_greatest_similarity(data, rate, engine_class, dataset_id, signature_dir=SIGNATURE_PARENT_DIR, n_tracks=10):
     sig_track = engine_class.extract_signature(data, rate)
-    signature_path = os.path.join(SIGNATURE_PARENT_DIR, dataset_id, engine_class.get_engine_identifier())
-    signature_files = glob.iglob(os.path.join(signature_path, "**/*.*"))
+    signatures = db.TrackSignature.query.filter(db.TrackSignature.audio_track.has(dataset_name=dataset_id))\
+        .filter_by(engine_class=engine_class.get_engine_identifier())
 
     h = []
     sim_vector = []
-    for sig_file in signature_files:
+    for sig in signatures:
+        sig_file = sig.path + ".npz"
         signature = numpy.load(sig_file)
         similarity = engine_class.measure_similarity(sig_track, signature)
         sim_vector.append(similarity)
 
         if len(h) < n_tracks:
-            heapq.heappush(h, (similarity, sig_file))
+            heapq.heappush(h, (similarity, sig))
 
         elif similarity > h[0][0]:
-            heapq.heappushpop(h, (similarity, sig_file))
+            heapq.heappushpop(h, (similarity, sig))
 
     h.sort()
     h = h[::-1]
@@ -38,7 +39,7 @@ def search_greatest_similarity(data, rate, engine_class, dataset_id, signature_d
         ret.append({
             "absolute_similarity": s[0],
             "standardized_similarity": stats.chi2.cdf(s[0], 5, mn, sd),
-            "signature_file": s[1]
+            "signature_file": s[1].path
         })
 
     return ret
--- webapp/preprocess.py
+++ webapp/preprocess.py
@@ -35,9 +35,9 @@ if __name__ == '__main__':
     engine_class = getattr(engine, sys.argv[2])
 
     # Save the engine
-    engine_model = database.EngineModel.query.filter_by(clazz=str(engine_class)).first()
+    engine_model = database.EngineModel.query.filter_by(clazz=engine_class.get_engine_identifier()).first()
     if engine_model is None:
-        engine_model = database.EngineModel(str(sys.argv[2]))
+        engine_model = database.EngineModel(engine_class.get_engine_identifier())
         database.db.session.add(engine_model)
 
     signatureset_identifier = engine_class.get_engine_identifier()
