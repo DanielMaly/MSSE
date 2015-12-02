e5867a168f996818d351aca217ba60cadcda244c
Daniel Maly <maly.dan@gmail.com>
2015-12-02 10:51:24 +0100

    Opraven bug při rovnosti dvou podobností

--- webapp/engine/search.py
+++ webapp/engine/search.py
@@ -9,6 +9,15 @@ import db.database as db
 SIGNATURE_PARENT_DIR = os.path.join("data", "signatures")
 
 
+class Similarity:
+    def __init__(self, signature, similarity_measure):
+        self.signature = signature
+        self.similarity_measure = similarity_measure
+
+    def __lt__(self, other):
+        return self.similarity_measure < other.similarity_measure
+
+
 def search_greatest_similarity(data, rate, engine_class, dataset_id, signature_dir=SIGNATURE_PARENT_DIR, n_tracks=10):
     sig_track = engine_class.extract_signature(data, rate)
     signatures = db.TrackSignature.query.filter(db.TrackSignature.audio_track.has(dataset_name=dataset_id))\
@@ -16,17 +25,18 @@ def search_greatest_similarity(data, rate, engine_class, dataset_id, signature_d
 
     h = []
     sim_vector = []
-    for sig in signatures:
-        sig_file = sig.path + ".npz"
+    for signature_record in signatures:
+        sig_file = signature_record.path + ".npz"
         signature = numpy.load(sig_file)
-        similarity = engine_class.measure_similarity(sig_track, signature)
-        sim_vector.append(similarity)
+        similarity_measure = engine_class.measure_similarity(sig_track, signature)
+        sim_vector.append(similarity_measure)
+        sim_object = Similarity(signature_record, similarity_measure)
 
         if len(h) < n_tracks:
-            heapq.heappush(h, (similarity, sig))
+            heapq.heappush(h, sim_object)
 
-        elif similarity > h[0][0]:
-            heapq.heappushpop(h, (similarity, sig))
+        elif sim_object > h[0]:
+            heapq.heappushpop(h, sim_object)
 
     h.sort()
     h = h[::-1]
@@ -37,9 +47,9 @@ def search_greatest_similarity(data, rate, engine_class, dataset_id, signature_d
     ret = []
     for s in h:
         ret.append({
-            "absolute_similarity": s[0],
-            "standardized_similarity": stats.chi2.cdf(s[0], 5, mn, sd),
-            "signature": s[1]
+            "absolute_similarity": s.similarity_measure,
+            "standardized_similarity": stats.chi2.cdf(s.similarity_measure, 5, mn, sd),
+            "signature": s.signature
         })
 
     return ret
